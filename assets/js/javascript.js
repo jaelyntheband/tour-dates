24e9cbd84867d944cde4f4dfeb5de07457697434
stevetotheizz0 <stephenjskilton@gmail.com>
2023-12-11 14:32:10 -0500

    Update javascript.js

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,9 @@
-
+// Get a reference to "myhref".
+var myhref = document.getElementByClassName("menu-link");
+// Get a reference to "myfield".
+var myfield = document.getElementById("click");
+// Attach an event listener to "myhref", which gets notifier when "myhref" is clicked.
+myhref.addEventListener("click", function() {
+    // "myhref" is click. So, uncheck "myfield"
+    myfield.checked = false;
+});
